8d100eedd1ce39ffda8f7fd3af2bb0bd404e07a5
shuklasp <shriprakashshukla8@gmail.com>
2025-01-05 19:14:04 +0530

    dynamic theme based on system

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,10 +1,17 @@
 import { ReactNode, createContext, useState } from "react"
 import { ThemeType, ThemeContextType } from "../Utils/Types"
 
-export const ThemeContext = createContext<ThemeContextType>(['light',()=>{}]);
+export const ThemeContext = createContext<ThemeContextType>(['light', () => { }]);
 
-const ThemeProvider = ({children}:{children:ReactNode}) => {
-    const [theme, setTheme] = useState<ThemeType>('light')
+const getTheme = (): ThemeType => {
+    const sessionTheme = sessionStorage.getItem('theme') as ThemeType | null
+    const systemTheme: ThemeType = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    return sessionTheme ? sessionTheme : systemTheme;
+}
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const [theme, setTheme] = useState<ThemeType>(getTheme())
+    sessionStorage.setItem('theme', theme)
     return (
         <ThemeContext.Provider value={[theme, setTheme]}>
             {children}
